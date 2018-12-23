2ebf9a490111f996aafbfc9d5a953854beabeaa6
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-12-23 15:28:42 +0530

    Create Arithmetic Operators.py

---
+++ hackerrank/Arithmetic Operators.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    
+    sum1 = a+b
+    diff = a-b
+    prod = a*b
+    print(sum1)
+    print(diff)
+    print(prod)
