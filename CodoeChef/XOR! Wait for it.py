347510875dee61755763bea65efedad9148f3706
Priyanka Madhwal <priyanka0701m@gmail.com>
2019-01-27 12:16:30 +0530

    Create XOR! Wait for it.py

---
+++ CodoeChef/XOR! Wait for it.py
@@ -0,0 +1,12 @@
+# https://www.codechef.com/problems/XORNEY
+
+'''
+If the count of Odd Numbers is even, Final XOR = Even ^ Even = Even
+If the count of Odd Numbers is Odd, Final XOR = Odd ^ Even = Odd
+'''
+
+for _ in range(int(input())):
+    L, R = map(int,input().split())
+    oddCount = (R - L) // 2
+    if R % 2 == 1 or L % 2 == 1: oddCount += 1 
+    print ('Even' if oddCount % 2 == 0 else 'Odd') 
